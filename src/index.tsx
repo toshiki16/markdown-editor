20f2469720a8ec009c8886b669a99e8ad7da912a
toshiki16 <toshiki6226@icloud.com>
2022-02-15 19:31:02 +0900

    2回目のコミット

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const Main = (<h1>Markdown Editordayo</h1>)
+
+render(Main, document.getElementById('app'))
+
+
+
+
+
+
+
+// const log = (message: string): void => {
+//     console.log(message)
+// }
+
+// log('Hello, Webpack + TypeScript!')
