924af6cd622b1e19de0153805bbe6aecf2da085f
toshiki16 <toshiki6226@icloud.com>
2022-02-23 23:05:00 +0900

    13回目のコミット

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components'
 import { Editor } from './pages/editor'
-
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom'
+import { History } from './pages/history'
 
 const GlobalStyle = createGlobalStyle`
     body * {
@@ -14,7 +20,15 @@ const GlobalStyle = createGlobalStyle`
 const Main = (
     <>
         <GlobalStyle />
-        <Editor />
+        <Router>
+            <Route exact path="/editor">
+                <Editor />
+            </Route>
+            <Route exact path="/history">
+                <History />
+            </Route>
+            <Redirect to="/editor" path="*" />
+        </Router>
     </>
 )
 
